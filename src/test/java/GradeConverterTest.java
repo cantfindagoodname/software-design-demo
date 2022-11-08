1774b9dc923d19bf8de8fae0dce56eeecf197d82
cantfindagoodname <f74087060@gs.ncku.edu.tw>
2022-11-08 11:21:15 +0800

    Initial Commit

---
+++ src/test/java/GradeConverterTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GradeConverterTest {
+    private GradeConverter converter;
+    @Before
+    public void setUp() throws Exception {
+        /* setup before testing*/
+        converter = new GradeConverter();
+    }
+
+    @Test
+    public void testConvert1() {
+        String expected = "A";
+        String actual = converter.convert(100);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvert2() {
+        String expected = "B";
+        String actual = converter.convert(90);
+        assertEquals(expected, actual);
+    }
+}
